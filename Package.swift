5c9851c084809a6e49bf214087574a01122db55e
Eugene Bokhan <eugenebokhan@protonmail.com>
2020-09-14 14:45:13 +0300

    introduce swiftpm support

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "ResourcesBridge",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13)
+    ],
+    products: [
+        .library(name: "ResourcesBridge",
+                 targets: ["ResourcesBridge"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/eugenebokhan/Bonjour.git",
+                 from: "2.0.1")
+    ],
+    targets: [
+        .target(name: "ResourcesBridge",
+                dependencies: ["Bonjour"],
+                linkerSettings: [
+                    .linkedFramework("MultipeerConnectivity"),
+                    .linkedFramework("CommonCrypto")
+                ])
+    ]
+)
--- ResourcesBridge.podspec
+++ ResourcesBridge.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = "ResourcesBridge"
-  s.version = "0.0.1"
+  s.version = "0.0.2"
 
   s.summary = "ResourcesBridge"
   s.homepage = "https://github.com/eugenebokhan/ResourcesBridge"
